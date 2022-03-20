16d2a142b0891a3750912b748a9f96b19f5c2b53
Alvaro Soria <alvarosoria.universidad@gmail.com>
2022-03-20 00:31:21 -0300

    Puntos d,e,f terminados

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -6,16 +6,49 @@ int cuadrado (int a){
 void cuadradoVoid (int *a){
     *a*=*a;
 }
+
+void invertir (int *a, int *b){
+    int aux;
+    aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+void ordenar (int *a, int *b){
+    if(*a > *b){
+        int aux;
+        aux = *b;
+        *b = *a;
+        *a = aux;
+    }
+}
 int main (){
 
     int num=2, num2=3;
-    printf("num: %d\n", num);
+    printf("\tnum: %d\n", num);
     num = cuadrado(num);
-    printf("\tDirección de memoria %p\nContrenido de la variable: %d\n\n", &num, num);
+    printf("\tDirección de memoria %p\n\tContrenido de la variable: %d\n\n", &num, num);
 
-    printf("num: %d\n", num2);
+    printf("\tnum: %d\n", num2);
     cuadradoVoid(&num2);
-    printf("\tDirección de memoria %p\nContrenido de la variable: %d\n\n", &num2, num2);
+    printf("\tDirección de memoria %p\n\tContrenido de la variable: %d\n\n", &num2, num2);
 
+
+    printf("\n\n\n#############################################\n\n\n");
+
+    int a=2,b=5;
+    printf("\ta: %d\n\tb: %d\n\n", a, b);
+    printf("\n\tINVERTIDO\n\n");
+    invertir(&a, &b);
+    printf("\ta: %d\n\tb: %d\n\n", a, b);
+
+    printf("\n\n\n#############################################\n\n\n");
+
+    
+    printf("\ta: %d\n\tb: %d\n\n", a, b);
+    printf("\n\tORDENADO\n\n");
+    ordenar(&a, &b);
+    printf("\ta: %d\n\tb: %d\n\n", a, b);
+    
     return 0;
 }
