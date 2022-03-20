9dcd584c8a8f6dd4c2eac8a5bbf5c1f43181e75f
Alvaro Soria <alvarosoria.universidad@gmail.com>
2022-03-19 22:13:22 -0300

    punto e,f,g,h,i,j completado

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main (){
-	printf("Hola mundo");
+	printf("Hola mundo\n\n");
+
+	int a=4;
+	int *p;
+	p = &a;
+	
+	printf("El contenido del puntero: %i\n\n", *p);
+	printf("Dirección de memoria almacenada por el puntero: %p\n\n", p);
+	printf("Dirección de memoria de la variable: %p\n\n", &a);
+	printf("Dirección de memoria del puntero: %p\n\n", &p);
+	printf("Tamaño de memoria utilizado por esa variable usando la función sizeof(): %i\n\n", sizeof(a));
+
+
 	return 0;
 }
+
+/* 
+El contenido del puntero
+Dirección de memoria almacenada por el puntero.
+Dirección de memoria de la variable.
+Dirección de memoria del puntero.
+Tamaño de memoria utilizado por esa variable usando la función sizeof().
+
+ */
